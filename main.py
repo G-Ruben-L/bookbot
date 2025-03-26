f42a024b765f087fe1319c010da522d238076ca9
G-Ruben-L <geerlig.lecluse@gmail.com>
2025-03-26 11:59:08 +0700

    Bookbot project, Chapter 2, lesson5 Refacotor

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
+from stats import count_words
+
+
 # open text file and read content
 def open_text(file_path):
     with open (file_path, "r", encoding="utf-8") as f:
         file_content = f.read()
     return file_content
 
-# split text into words
-def split_text(text):
-        word_count = len(text.split())
-        return word_count
 
 
 def main():
     text = open_text('books/frankenstein.txt')
-    word_count = split_text(text)
+    word_count = count_words(text)
     print(f'{word_count} words found in the document')
 
 
---
+++ stats.py
@@ -0,0 +1,4 @@
+# split text into words and count words
+def count_words(text):
+        word_count = len(text.split())
+        return word_count
