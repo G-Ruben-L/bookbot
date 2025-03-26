1df04e6f4de8b0a8b2884e768ae6cc8e68b7d2a2
G-Ruben-L <geerlig.lecluse@gmail.com>
2025-03-26 14:32:58 +0700

    Character count function added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import count_words
-
+from stats import count_characters
 
 # open text file and read content
 def open_text(file_path):
@@ -7,12 +7,14 @@ def open_text(file_path):
         file_content = f.read()
     return file_content
 
-
-
+# call and print word count and counts per character
 def main():
     text = open_text('books/frankenstein.txt')
     word_count = count_words(text)
     print(f'{word_count} words found in the document')
+    character_count = count_characters(text)
+    for char, count in character_count.items():
+        print(f'{char}: {count}')
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@
 def count_words(text):
         word_count = len(text.split())
         return word_count
+
+# counting the occurrence of each character in text
+def count_characters(text):
+        character_count = {}
+        for char in text.lower():
+                if char not in character_count:
+                        character_count.update({char: 1})
+                else:
+                        character_count[char] += 1
+
+        return character_count
